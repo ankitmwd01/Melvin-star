3fff54054112b2e9684a1ed8c9de9ec1c6a3ddba
Ankit Mewada <ankitmwd01@gmail.com>
2023-03-28 20:02:16 +0530

    Frotend, Later we add Backend

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Melvin Star</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/channel/UCMtLL_jiqhP3Bu48NmeG8Gw" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/ankit.mwd/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/ankitmwd01" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>Melvin Star</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+          Melvin Tech is a leading technology company that specializes in providing innovative solutions for businesses and individuals. Our mission is to simplify complex technological challenges and make them accessible to everyone.
+
+At Melvin Tech, we offer a range of products and services that are designed to improve the efficiency and productivity of our clients. From software development and web design to cloud solutions and cybersecurity, our team of experts has the expertise to deliver customized solutions that meet the specific needs of our clients.
+
+One of our flagship products is our state-of-the-art project management software that allows businesses to streamline their workflows, collaborate more effectively, and stay on top of deadlines. Our software is designed to be user-friendly and intuitive, so that anyone can use it regardless of their technical background.
+
+In addition, we also provide top-notch cybersecurity solutions to help businesses protect their sensitive data and prevent unauthorized access. We employ cutting-edge technologies and best practices to ensure that our clients' data is secure and protected at all times.
+
+
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
